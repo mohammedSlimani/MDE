cd72c76ef997520490aa906a679839a7fb642a86
smed <slimani.med.21@gmail.com>
2019-12-09 21:12:57 +0100

    Refactor

--- LctoALC/index.js
+++ LctoALC/index.js
@@ -11,31 +11,26 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use( express.static( "public" ) );
+app.use(express.static("public"));
 
-function updateArray(diagArrElement, diagArrElement2,array) {
-
-let s = { Ord:  diagArrElement['action'] , Inh : diagArrElement2['action']};
-
+function updateArray(diagArrElement, diagArrElement2, array) {
+    let s = {Ord: diagArrElement['action'], Inh: diagArrElement2['action']};
     array.forEach((item) => {
-       if (item['from'] === diagArrElement['from'] || item['from'] ===diagArrElement['to'] || item['from'] ===diagArrElement2['from']) {
+        if (item['from'] === diagArrElement['from'] || item['from'] === diagArrElement['to'] || item['from'] === diagArrElement2['from']) {
             item['from'] = s;
-       }
-       if (item['to'] === diagArrElement['from'] || item['to'] ===diagArrElement['to'] || item['to'] ===diagArrElement2['from']){
-           item['to'] = s;
-       }
+        }
+        if (item['to'] === diagArrElement['from'] || item['to'] === diagArrElement['to'] || item['to'] === diagArrElement2['from']) {
+            item['to'] = s;
+        }
     });
-
-
-
-return array;
+    return array;
 }
 
 function fromLCtoLAC(lc) {
     let myJson = lc;
     //console.log(myJson);
-    var diagArr = [];
-    var diagArr2 = [];
+    let diagArr = [];
+    let diagArr2 = [];
     //myJson = JSON.parse(myJson);
     myJson.forEach((item) => {
             let action = item['action'];
@@ -70,47 +65,50 @@ function fromLCtoLAC(lc) {
 
 }
 
-app.post('/Draw',(req,res,next)=> {
-    let myJson = req.body.uml;
-   //console.log(myJson);
+app.post('/Draw', (req, res, next) => {
+        let myJson = req.body.uml;
+        //console.log(myJson);
 
-    var gen = plantuml.generate(myJson);
-    gen.out.pipe(fs.createWriteStream("output-file.png"));
-}
+        var gen = plantuml.generate(myJson);
+        gen.out.pipe(fs.createWriteStream("output-file.png"));
+    }
 );
 
-app.get('/Home',(req,rep,next) => {
+app.get('/Home', (req, rep, next) => {
     rep.render('home.ejs');
-})
-    .get('/Results',(req,rep,next) => {
+    })
+    .get('/Results', (req, rep, next) => {
         rep.render('result.ejs');
     });
 
 
-app.post('/getInfo', async (req,rep,next) => {
-    console.log('LC PLANTUML  : ',req.body.plantuml);
-    console.log();console.log();
-    const resp = await axios.post('http://tromastrom.pythonanywhere.com/parse', {"uml":req.body.plantuml});
-    console.log('LC JSON : ',resp);
-    console.log();console.log();
+app.post('/getInfo', async (req, rep, next) => {
+    console.log('LC PLANTUML  : ', req.body.plantuml);
+    console.log();
+    console.log();
+    const resp = await axios.post('http://tromastrom.pythonanywhere.com/parse', {"uml": req.body.plantuml});
+    console.log('LC JSON : ', resp);
+    console.log();
+    console.log();
     const resp2 = await axios.post('http://tromastrom.pythonanywhere.com/inverse', fromLCtoLAC(resp.data.lc));
-   //let todraw =  resp2.data.uml;
-   //console.log('Plaint Uml ToDraw : ' , todraw);
+    //let todraw =  resp2.data.uml;
+    //console.log('Plaint Uml ToDraw : ' , todraw);
     console.log();
     console.log();
-   // var gen = plantuml.generate(todraw);
+    // var gen = plantuml.generate(todraw);
     //gen.out.pipe(fs.createWriteStream("LAC.png"));
     console.log("Ocl Contraints : ", req.body.ocl);
     console.log();
     console.log();
     console.log("DC", fromLCtoLAC(resp.data.lc));
-    console.log();console.log();
-    const  resp3  = await axios.post('https://smed-dc.glitch.me/parse',{
+    console.log();
+    console.log();
+    const resp3 = await axios.post('https://smed-dc.glitch.me/parse', {
         "source": fromLCtoLAC(resp.data.lc),
-        'cdt' : req.body.ocl
+        'cdt': req.body.ocl
     });
-console.log("resp3", resp3.data.result);
+    console.log("resp3", resp3.data.result);
 });
-app.listen(8080, ()=>{
+app.listen(8080, () => {
     console.log("Front End Started at http://localhost:8080/home")
 });
